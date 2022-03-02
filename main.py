3b16608186597d946e0261bde52f488d3c637ac9
harryabc <harryabc@sina.com>
2022-03-02 17:07:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,38 +12,38 @@ def goods():
     data = [
         {
             "id": 1,
-            "name": "Apple iPhone 13",
+            "name": "Apple iPhone 13 Pro Max 256GB",
             "image": "https://img1.360buyimg.com/img/jfs/t1/206327/14/6656/37878/614182f3Ec0388cb1/9c624f9cdc0ef6b0.jpg",
             "price": "9799.00",
-            "ware": "京东自营"
-        },
-        {
-            "id": 2,
-            "name": "Apple iPhone 13",
-            "image": "https://img10.360buyimg.com/img/jfs/t1/82549/18/16596/35941/6141c60bE7c2fab8a/e619bc383c1f524a.jpg",
-            "price": "8799.00",
             "ware": "天猫"
         },
         {
+            "id": 2,
+            "name": "Apple iPhone 13 Pro 套装 256GB",
+            "image": "https://img30.360buyimg.com/img/jfs/t1/213424/38/890/63246/616d3d24E1b79e093/ccfb3724000eb2f3.jpg",
+            "price": "12899.00",
+            "ware": "淘宝"
+        },
+        {
             "id": 3,
-            "name": "小米Redmi 红米note9",
-            "image": "https://img10.360buyimg.com/img/jfs/t1/168666/4/23738/170302/613ca1edE34ec60b1/f3c2ba7bbf84a149.jpg",
-            "price": "1099.00",
+            "name": "Apple iPhone 13 Pro 金色",
+            "image": "https://img10.360buyimg.com/img/jfs/t1/82549/18/16596/35941/6141c60bE7c2fab8a/e619bc383c1f524a.jpg",
+            "price": "8799.00",
             "ware": "淘宝"
         },
         {
             "id": 4,
-            "name": "ROG游戏手机5s",
-            "image": "https://img11.360buyimg.com/img/jfs/t1/192403/9/21433/117581/6130856cE2b75c0c7/fa706f41b56b77c6.jpg",
-            "price": "4499.00",
-            "ware": "淘宝"
+            "name": "Apple iPhone 13 Pro",
+            "image": "https://img20.360buyimg.com/img/jfs/t1/145583/7/20347/204802/614beca5Effadf187/e1a790c81b86aecb.jpg",
+            "price": "9799.00",
+            "ware": "京东"
         },
         {
             "id": 5,
-            "name": "vivo iQOO 7",
-            "image": "https://img10.360buyimg.com/img/jfs/t1/170012/31/21215/75108/61554ac7E0c731e73/d73ec930b6307bd4.jpg",
-            "price": "2998.00",
-            "ware": "淘宝"
+            "name": "Apple iPhone 13 Pro",
+            "image": "https://img14.360buyimg.com/img/jfs/t1/202422/15/6977/26859/6143579bE79557c8c/b72c71338a403952.jpg",
+            "price": "8998.00",
+            "ware": "拼多多"
         }
     ]
 
